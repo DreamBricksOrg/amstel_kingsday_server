2449d111074ac55d0da5bd07dc7e6a9f057000cc
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2025-04-14 15:15:48 -0300

    feat: add routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,50 @@ app = Flask(__name__)
 def home():
     return "alive"
 
+@app.route('/')
+def index():
+    return render_template('toque_tela.html')
+
+@app.route('/abrircamera')
+def abrir_camera():
+    return render_template('abrir_camera.html')
+
+@app.route('/agedenied')
+def age_denied():
+    return render_template('age_denied.html')
+
+@app.route('/agegate')
+def age_gate():
+    return render_template('age_gate.html')
+
+@app.route('/agegatedata')
+def age_gate_data():
+    return render_template('age_gate_data.html')
+
+@app.route('/download')
+def download():
+    return render_template('download.html')
+
+@app.route('/downloadcompartilhar')
+def download_compartilhar():
+    return render_template('download_compartilhar.html')
+
+@app.route('/downloadqrcode')
+def download_qrcode():
+    return render_template('download_qrcode.html')
+
+@app.route('/fotoprocessada')
+def foto_processada():
+    return render_template('foto_processada.html')
+
+@app.route('/politicaprivacidade')
+def politica_priva():
+    return render_template('politica_priva.html')
+
+@app.route('/termosuso')
+def termos_uso():
+    return render_template('termos_uso.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", ssl_context='adhoc')
+    app.run(debug=True, host="0.0.0.0")
