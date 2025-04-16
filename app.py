97329d05a5250491deff24f0322a8d5e98c2505f
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2025-04-16 15:36:51 -0300

    feat: add tablet routes and add 'Restart' button

--- app.py
+++ app.py
@@ -14,46 +14,89 @@ def home():
 def index():
     return render_template('toque_tela.html')
 
+@app.route('/tablet/')
+def tablet_index():
+    return render_template('toque_tela.html')
+
 @app.route('/abrircamera')
 def abrir_camera():
+    return render_template('abrir_camera.html', api_url=parameters.API_URL)
+
+@app.route('/tablet/abrircamera')
+def tablet_abrir_camera():
     return render_template('abrir_camera.html', api_url=parameters.API_URL)
 
 @app.route('/agedenied')
 def age_denied():
     return render_template('age_denied.html')
 
+@app.route('/tablet/agedenied')
+def tablet_age_denied():
+    return render_template('age_denied.html')
+
 @app.route('/agegate')
 def age_gate():
+    return render_template('age_gate.html')
+
+@app.route('/tablet/agegate')
+def tablet_age_gate():
     return render_template('age_gate.html')
 
 @app.route('/agegatedata')
 def age_gate_data():
     return render_template('age_gate_data.html')
 
+@app.route('/tablet/agegatedata')
+def tablet_age_gate_data():
+    return render_template('age_gate_data.html')
+
 @app.route('/download')
 def download():
+    return render_template('download.html')
+
+@app.route('/tablet/download')
+def tablet_download():
     return render_template('download.html')
 
 @app.route('/downloadcompartilhar')
 def download_compartilhar():
     return render_template('download_compartilhar.html', api_url=parameters.API_URL)
 
+@app.route('/tablet/downloadcompartilhar')
+def tablet_download_compartilhar():
+    return render_template('download_compartilhar.html', api_url=parameters.API_URL)
+
 @app.route('/downloadqrcode')
 def download_qrcode():
-    return render_template('download_qrcode.html')
+    return render_template('download_qrcode.html', api_url=parameters.API_URL)
+
+@app.route('/tablet/downloadqrcode')
+def tablet_download_qrcode():
+    return render_template('download_qrcode.html', api_url=parameters.API_URL)
 
 @app.route('/fotoprocessada')
 def foto_processada():
+    return render_template('foto_processada.html', api_url=parameters.API_URL)
+
+@app.route('/tablet/fotoprocessada')
+def tablet_foto_processada():
     return render_template('foto_processada.html', api_url=parameters.API_URL)
 
 @app.route('/politicaprivacidade')
 def politica_priva():
     return render_template('politica_priva.html')
 
+@app.route('/tablet/politicaprivacidade')
+def tablet_politica_priva():
+    return render_template('politica_priva.html')
+
 @app.route('/termosuso')
 def termos_uso():
     return render_template('termos_uso.html')
 
+@app.route('/tablet/termosuso')
+def tablet_termos_uso():
+    return render_template('termos_uso.html')
 
 if __name__ == '__main__':
     context = ('fullchain.pem', 'privkey.pem')
